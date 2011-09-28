84cae4fa67d79ef946ab3f9bf55af0eb00ce5b25
Linus Oleander <linus@oleander.nu>
2011-09-28 23:52:39 +0200

    Implementing advanced mode

--- worker.rb
+++ worker.rb
@@ -16,18 +16,22 @@ optparse = OptionParser.new do|opts|
 
   # Define the options, and what they do
   options[:mode] = :simple
-  opts.on( '-m', '--mode', "Start up mode [simple, beginner, expert]" ) do |mode|
-    options[:verbose] = mode.to_sym
+  opts.on( '-s', '--simple', "Simple mode" ) do |mode|
+    options[:mode] = :simple
   end
 
+  opts.on( '-a', '--advanced', "Advanced mode" ) do |mode|
+    options[:mode] = :advanced
+  end
+  
   options[:loop] = false
   opts.on( '-l', '--loop', "Should be continue forever?" ) do |mode|
     options[:loop] = !! mode
   end
   
   options[:time] = 20
-  opts.on( '-t', '--time', "Time constant" ) do |time|
-    options[:time] = time.to_i
+  opts.on( '-t', '--time', "Set time to normal, otherwise 20" ) do |time|
+    options[:time] = 1
   end
   
   # This displays the help screen, all programs are
@@ -38,17 +42,40 @@ optparse = OptionParser.new do|opts|
   end
 end
 
+optparse.parse!
+
 def sleep_for(sec, time)
   sleep(sec.to_f / time)
 end
 
 modes = {}
+
+# Simple mode, nothing goes wrong
 modes[:simple] = lambda do |station|
   if station.first_station?
     debug "This (#{station.name}) is the first station."
   else
     debug "Leaving #{station.previous_station.name}, sending 'did_leave_station' to server."
     debug "Heading towards #{station.name}, there in #{station.time_from_prev_station} seconds."
+    sleep_for(station.time_from_prev_station, options[:time])
+  end
+end
+
+# Advanced mode, small things goes wrong, nothing major.
+modes[:advanced] = lambda do |station|
+  if station.first_station?
+    debug "This (#{station.name}) is the first station."
+  else
+    debug "Leaving #{station.previous_station.name}, sending 'did_leave_station' to server."
+    
+    if rand(2) % 2 == 0
+      debug "Ooops, train is late #{late = rand(29)} seconds, sending update to client.", :red
+      time = station.time_from_prev_station + late
+    else
+      time = station.time_from_prev_station
+    end
+    
+    debug "Heading towards #{station.name}, there in #{time} seconds."
     sleep_for(station.time_from_prev_station, options[:time])
   end
 end
