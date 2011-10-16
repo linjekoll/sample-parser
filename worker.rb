f606e09e45a62ddd535539aaadb1806d7958803f
Linus Oleander <linus@oleander.nu>
2011-10-16 15:35:45 +0200

    Implementing parts of the event class

--- worker.rb
+++ worker.rb
@@ -2,8 +2,6 @@ require "jsonify"
 require "colorize"
 require "optparse"
 require_relative "config"
-
-# attr_reader :name, :id, :time_from_prev_station, :destination_station, :origin_station, :station_id
  
 def debug(message, color = :yellow)
   puts "%s %s\n\n" % ["==>".black, message.to_s.send(color)]
@@ -54,11 +52,29 @@ end
 
 modes = {}
 
+# event: "did_leave_station",
+# origin_station: 8998235,
+# destination_station: 898345,
+# station_id: 123123,
+# arrival_time: 1235,
+# alert_message: "oops!",
+# line_id: 2342,
+# provider_id: 23,
+
+event = Event.new(provider: 1, line_id: 2)
+
 # Simple mode, nothing goes wrong
 modes[:simple] = lambda do |station|
   if station.first_station?
     debug "This (#{station.name}) is the first station."
   else
+    event.set({
+      journey_id: station.id, 
+      arrival_time: station.time_to_next_station, 
+      destination_station: station.destination_station,
+      station_id: station.sid
+    }).did_leave_station.push!
+    
     debug "Leaving #{station.previous_station.name}, sending 'did_leave_station' to server."
     debug "Heading towards #{station.name}, there in #{station.time_from_prev_station} seconds."
     sleep_for(station.time_from_prev_station, options[:time])
