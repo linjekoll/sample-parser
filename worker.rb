d908cd07b641a8f5fa4711b086dfd280f7bec550
Linus Oleander <linus@oleander.nu>
2011-10-17 17:17:47 +0200

    Removing unused modes

--- worker.rb
+++ worker.rb
@@ -81,62 +81,6 @@ modes[:simple] = lambda do |station|
   end
 end
 
-# Advanced mode
-# - trains might be late
-modes[:advanced] = lambda do |station|
-  if station.first_station?
-    debug "This (#{station.name}) is the first station."
-  else
-    debug "Leaving #{station.previous_station.name}, sending 'did_leave_station' to server."
-    
-    if rand(2) % 2 == 0
-      debug "Ooops, train is late #{late = rand(29)} seconds, sending update to client.", :red
-      time = station.time_from_prev_station + late
-    else
-      time = station.time_from_prev_station
-    end
-    
-    debug "Heading towards #{station.name}, there in #{time} seconds."
-    sleep_for(station.time_from_prev_station, options[:time])
-  end
-end
-
-# Expert mode
-# - warnings are being send out
-# - trains might be late or too early
-# - trains might stop at stations
-modes[:expert] = lambda do |station|
-  if station.first_station?
-    debug "This (#{station.name}) is the first station."
-  else
-    debug "Leaving #{station.previous_station.name}, sending 'did_leave_station' to server."
-    
-    offset = 0
-    
-    if rand(5) == 0
-      sleep_for(late = rand(29), options[:time])
-      debug "Ooops, train is late #{late} seconds, sending update to client.", :red
-    elsif rand(5) == 0
-      offset = -1 * rand(20)
-      debug "Ooops, train is to early (#{offset}) seconds.", :red
-    end
-    
-    time = station.time_from_prev_station + offset
-    
-    debug "Heading towards #{station.name}, there in #{time} seconds."
-    sleep_for(time, options[:time])
-    
-    if rand(5) == 0
-      debug "Stopping at #{station.name} for #{late = rand(100)} seconds.", :red
-      sleep_for(late, options[:time])
-    end
-    
-    if rand(10) == 0
-      debug "Sending alert to client!", :red
-    end
-  end
-end
-
 debug "Starting in #{options[:mode]} mode, loop is #{options[:loop]}, time constant is set to #{options[:time]}.", :green
 
 begin
